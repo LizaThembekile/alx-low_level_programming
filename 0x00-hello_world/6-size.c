2b601b5668783636d0e2e7837376ef29899b53d5
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-15 12:15:04 -0700

    6sizec

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of a char: %d byte(s)\n", sizeof(i));
+	printf("Size of a char: %d byte(s)\n", sizeof(li));
+	printf("Size of a char: %d byte(s)\n", sizeof(lli));
+	printf("Size of a char: %d byte(s)\n", sizeof(f));
+	return (0);
+}
