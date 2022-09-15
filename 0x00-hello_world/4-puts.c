d9f1e03d6e861d97789f2e98dde21925f4a0f383
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-15 11:57:59 -0700

    4putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, followed by a new line
+ * main - Printing
  *
- * Retur: Always 0
+ * Return: Always 0
  */
 
 int main(void)
