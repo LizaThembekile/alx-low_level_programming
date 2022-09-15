36425ff9ed30e891ad1282e4d626cb83e6b92e9e
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-15 12:45:48 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - Prints text
+ * main - Prints a text
  *
  * Return: Always 1
  */
@@ -10,5 +10,7 @@ int main(void)
 
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return(1);
+	
+	return (1);
 }
+
