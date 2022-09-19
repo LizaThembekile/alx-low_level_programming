e981dc2bbb59ced6bb40270e23c6ee27e6067ce3
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-19 01:54:36 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -7,10 +9,9 @@
 int main(void)                                                                       {                  
 char low;
 
-for (low = 'a' ; low <= 'z' ; low++)
+  for (low = 'a' ; low <= 'z' ; low++)
 	putchar(low);
-
         putchar("\n");
 
-return (0);
+  return (0);
 }
