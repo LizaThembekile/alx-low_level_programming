7ab63d5779fdadcff951a7be652c04f94c42eb95
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-19 08:37:44 -0700

    hi

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
-
 /**
 * main - prints putchar
+*
 * Return: 0
 */
 int main(void)
@@ -15,6 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 } 
