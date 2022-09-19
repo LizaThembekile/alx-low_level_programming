21f927c093a05d537c22c6410541311067513908
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-19 10:00:51 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 
 /**
 * prints the alphabet, in lowercase, followed by a new line
-*
 */
 
 void print_alphabet(void)
 {
 	char low;
-	for (low = 'a' ; low <= 'z' ; low++)
-		_putchar(low);
-	_putchar('\n')
 
+	for (low = 'a' ; low <= 'z' ; low++)
+	{
+		_putchar(low);
+	}
+		_putchar('\n');
 }
