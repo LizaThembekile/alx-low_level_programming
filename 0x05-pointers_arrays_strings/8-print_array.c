f85cddb1804524de4854b4819918ac0387962550
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-21 08:19:12 -0700

    fied

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,12 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-	{
-		printf(",");
-		printf(" ");
-	}
-	printf('\n');
+{
+printf("%d" , a[i]);
+if (i != n - 1)
+{
+printf(",");
+printf(" ");
+}
+printf('\n');
 }
