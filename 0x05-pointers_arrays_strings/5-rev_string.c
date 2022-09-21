883cc2f963ff2131e542925a492a4edab7961650
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-21 00:20:45 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string -> reversing a string
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, l1, l2;
+
+	l1 = 0;
+	l2 = 0;
+
+	while (s[l1] != '\0')
+		l1++;
+
+	l2 = l1 - 1;
+	for (i = 0; i < l1 / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[l2];
+		l2 -= 1;
+	}
+}
