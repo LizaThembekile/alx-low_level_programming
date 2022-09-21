5ed4341985ee52da7b16495933af90ebb6b8ed42
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-21 06:47:12 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
  * print_rev -> prints string in reverse
  * @s: the string printed in reverse
  */
-
 void print_rev(char *s)
 {
-	int c = 0
-	while (s[c] != '\0')
-        {
-		c++;
-	}
-	for (c -=1; c >= 0; c--)
+	int c;
+	
+	for (c = 0; s[c] != '\0'; c++)
+		;
+	for (c = c - 1; s[c] != '\0'; c--)
 	{
 		_putchar(s[c]);
 	}
