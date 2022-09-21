6a52ca89493266fbb6878bb16a8fece71bfcc6e1
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-20 21:04:15 -0700

    task23

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * Description:prints a string
+ * On success: returns no error
+ */
+void _puts(char *str)
+{
+
+	for (; *str != '\0' ; str++)
+	{
+	putchar(*ste);
+	}
+	putchar('\n');
+}
