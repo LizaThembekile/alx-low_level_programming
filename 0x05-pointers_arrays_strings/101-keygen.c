587bdde1cb0ccb7f32585870f74d05d3069aa61a
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-21 09:54:31 -0700

    tmando

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates random valid password
+ * Return: 0 (Succecc)
+ *
+ */
+int main(void)
+{
+	int passw, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += passw;
+		printf("%c" , sum);
+	}
+	printf("%c" , 27772 - sum);
+
+	return (0);
+}
