8e8b24746985067ffe09e5682ed476b371fbd849
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 06:55:19 -0700

    fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,4 @@
-#include "main.h"                                                                   
+#include "main.h"                                                  
 
 /**
 * reverse_array -> reversing array
@@ -8,11 +8,10 @@
 void reverse_array(int *a, int n)
 {
 int *p, i, aux, k;
-
 p = a;
-for (i =0; i < n; i++)
+for (i = 0; i < n; i++)
 p++;
-for (k = 0; k < n / 2; k++)
+for (k = 0; k < i / 2; k++)
 {
 aux = a[k];
 a[k] = *p;
