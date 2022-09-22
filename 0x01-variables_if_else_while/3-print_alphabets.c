220937ae142fcedd07c67244daf59544517aaa65
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 14:24:44 -0700

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,19 @@
-nclude <stdio.h>                                                                  
-#include <stdlib.h>                                                                 
-#include <time.h>                                                                   
-/**                                                                                 
- * main - Entry point                                                               
- *                                                                                  
- * Return: Always 0 (Success) 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char low;
+char low;
 
-	for (low = 'a' ; low <= 'z' ; low++)
-		putchar(low);
-	for (low = 'A' ; low <= 'Z' ; low++)
-		putchar(low);
-                putchar('\n');
-
-        return (0);
-}	
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+for (low = 'A'; low <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
