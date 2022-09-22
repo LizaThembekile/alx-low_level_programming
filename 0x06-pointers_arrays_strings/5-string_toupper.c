9679be66c9bd1e823312ca1a56b433babf5f2ad4
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 07:04:26 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper ->  convert string to upper case
+ * @x: string parameter
+ * Return: string
+ */
+char *string_toupper(char *)
+{
+	i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[1] -32;
+		i++;
+	}
+	return (x);
+}
