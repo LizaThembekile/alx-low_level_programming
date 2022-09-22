1d72bb5b2469fcaf3abdc67e58b5039cb7df70ad
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 06:18:57 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp -> Comparing
+* @s1: 1st param
+* @s2: 2nd param
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+int result;
+
+result = strcpmp(s1, s2);
+return (result);
+}
