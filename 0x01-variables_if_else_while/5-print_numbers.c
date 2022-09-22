b3100f6addcde8ed30eac2def3fd020efdd8dc09
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 14:45:20 -0700

    fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int;
+
+for (i = 0; i < 10; i++)
+printf(%d", i);
+putchar('\n');
+return (0);
+}
