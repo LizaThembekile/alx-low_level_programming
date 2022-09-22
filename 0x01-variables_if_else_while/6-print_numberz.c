e90015669be2732fcf6c96b81dd4d62de911ddcd
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 14:53:06 -0700

    fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* mai - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int d;
+for (d = '0'; d<= '9'; d++)
+putchar(d);
+puctcahr('\n');
+return (0);
+}
