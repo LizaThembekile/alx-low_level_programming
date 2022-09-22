3a8afef83f46d77ebab3d9666e0923ce97b40d89
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 15:45:50 -0700

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 {
 int d;
 
-for (d = 0; d < 10; d++)
+for (d = 0; d < 90; d++)
 {
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
 
-if (d != 9)
+if (d != 89)
 {
 putchar(',');
 putchar(' ');
