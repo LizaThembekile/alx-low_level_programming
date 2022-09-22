9e9ebd3ea1ed5afc2b9c77ba2454e7f0ff26f81c
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 05:36:28 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,18 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	i = 0;
-	j = 0;
+i = 0;
+j = 0;
 
-	while (dest[i] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+while (dest[i] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
