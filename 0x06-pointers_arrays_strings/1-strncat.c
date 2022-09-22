e7eb9dd7aebc913cd2f53f057edb1944a88aa06c
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 05:45:27 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,15 @@
+#include "main.h"
+#include <string>
 
+/**
+ * _strncat -> function append
+ * @dest: 1sr param
+ * @src: 2nd param
+ * @n: 3rd param
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest. src, n);
+	return (dest);
+}
