135bc3be79282507d53eee8f64f88da83103733b
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 07:50:48 -0700

    task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] -  32;
 		i++;
 	}
 	return (str);
