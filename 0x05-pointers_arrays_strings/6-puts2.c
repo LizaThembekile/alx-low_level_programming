ae2b70abfe5e5a13c316ae51ba9e07498cb1a54b
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 04:27:21 -0700

    fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,10 @@
  */
 void puts2(char *str)
 {
-int i;
+int len, i
 
-i = 0;
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		i++;
-	}
-putchar('\n');
+	len = strlen(str);
+for (i = 0; i < len; i += 2)
+	_putchar(str[i]);
+_putchar('\n');
 }
