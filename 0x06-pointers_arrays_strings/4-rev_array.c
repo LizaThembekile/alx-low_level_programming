6c75c9c20cce3f134b96cd4585455f15ea92387e
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 06:45:44 -0700

    fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,16 +7,16 @@
 */
 void reverse_array(int *a, int n)
 {
-int p, i, aux, k;
+int *p, i, aux, k;
 
-p = a;
+*p = a;
 for (i =0; i < n; i++)
-p++;
+*p++;
 for (k = 0; k < n / 2; k++)
 {
 aux = a[k];
-a[k] = p;
-p = aux;
-p--;
+a[k] = *p;
+*p = aux;
+*p--;
 }
 }
