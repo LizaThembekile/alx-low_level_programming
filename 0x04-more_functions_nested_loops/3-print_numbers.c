f6062f592a78b8dcec6916db64c110f86ce3c1e1
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-23 13:41:16 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ *
+ * Return: 0, and the print stated on the stdout
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 0; x++)
+		_putchar(x + '0');
+	_putchar(('\n');
+}
