b8a9d16e333a655ec507b7907fdf3de8274de9b0
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 17:02:44 -0700

    fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,18 @@
-include "main.h"
+#include "main.h"
 
 /**
  * string_toupper - changer lower case to upper case
  * @x: string to be changed
- *
  * Return: Addreaa to string
  */
 char *string_toupper(char *x)
 {
-
 	int i = 0;
 
 	while (x[i])
 	{
-		if (x[i] >= 'a'; x[i] <= 'z')
-			x[i] -= 32;
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
 		i++;
 	}
 	return (x);
