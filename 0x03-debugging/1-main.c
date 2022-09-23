1e7b6e411cdbf79bb646aa643eb95a5a50dc64f9
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 22:08:07 -0700

    fic

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,6 @@
-#**
+#include <stdio.h>
+
+/**
 * main - causes an infinite loop
 * Return: 0
 */
@@ -11,12 +13,6 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/**
-* while (i < 10)
-*{
-*
-*}
-*/
-printf(Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
