5c5d860441fa9170ef59a14d804827f052e96025
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-23 08:38:24 -0700

    fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void positive_or_negative(int i);
-int largest_number(int a, int d, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-void print_times_table(int n);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
