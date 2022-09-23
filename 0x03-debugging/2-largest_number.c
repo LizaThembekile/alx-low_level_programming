784b0507a9649376f2c36ef23f71e4ee4f5691ac
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-23 08:21:02 -0700

    fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,20 +7,28 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
+
 int largest;
+
 if (a > b && a > c)
 {
+
 largest = a;
+
 }
 else if (b > c && b > a)
 {
-largest = b;
+
+	largest = b;
 }
 else
 {
-largest = c;
+
+	largest = c;
 }
+
 return (largest);
 }
