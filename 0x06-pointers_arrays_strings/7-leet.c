6937dc1c790a1f68f1ea4390bc3ac8c2559087c0
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-22 18:19:08 -0700

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,42 @@
 #include "main.h"
 
-/** leet -> a leet function
+/**
+ * leet -> a leet function
  * @x: parameter
  * Return: A string
  */
 char *leet(char *x)
 {
-	int a = 0, b = 0, l = 5
-	char tr[5] = {'A', 'E', '0', 'T', 'L'};
-	char trw[5] = {'4', '3', '0', '7', '1'};
+	int i = 0;
 
-	while (x[a])
+	while (x[i] != '\0')
 	{
-		b = 0;
-
-		while (b < l)
-		{
-		if (x[a] == tr[b] || x[a] - 32 == tr[b])
-			x[a] = trw[b];
-		b++;
-		}
-		a++;
+		str[i] = transform(x[i]);
+		i++;
 	}
-	return (x); 
+	return (x);
+}
+/**
+ * transform - hlper function
+ * @x: char to be encoded
+ *
+ * Return: the encoded char
+ */
+char transform(char x)
+{
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 8)
+	{
+		if (x == mapping_low[i] || x == mapping_upper[i])
+		{
+			replacement = i + '0';
+			break
+		}
+		i++;
+	}
+	return (replacement);
 }
