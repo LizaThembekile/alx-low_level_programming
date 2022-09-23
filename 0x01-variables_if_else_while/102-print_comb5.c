3627b77d90a1baa0413387ff21ae5c87b177024e
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-23 00:21:18 -0700

    fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ while (num1 <= 99)
 			putchar((num1 % 10) + 48);
 			putchar(' ');
 			putchar((num2 / 10) + 48);
-			putchar((num2 % 10) + 49);
+			putchar((num2 % 10) + 48);
 
 			if (num1 != 98 || num2 != 99)
 			{
