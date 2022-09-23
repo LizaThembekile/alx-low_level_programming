ac55af074fb39abb8f1241b60aa731679a4e1204
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-23 15:52:13 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,28 +11,17 @@ int main(void)
 	int x;
 
 	for (x = 1; x <= 100; x++)
-
-{
-	if (x % 3 != 0 && x % 5 != 0)
 	{
-		printf("%d", x);
+		if (x == 100)
+			printf("Buzz");
+		else if (x % 3 == 0 && x % 5 ==)
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("FIZZ");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", x);
 	}
-	else if (x % 3 == 0 && x % 5 == 0)
-{
-		printf("FizzBuzz");
-	}
-	else if (x % 3 == 0)
-	{
-		printf("Fizz");
-	}
-	else if (x % 5 == 0)
-	{
-		printf("Buzz");
-	}
-
-	if (x < 100)
-		printf(" ");
-}
 	_putchar('\n');
-	return (0);
 }
