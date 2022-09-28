99ad50b8de7d390ca7918d16480160284f9bcd94
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-28 14:31:10 -0700

    fix

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* strlen_no_wilds - Returns the length of a string
+*
+*@str: the string to be measured
+*
+* Return: the length.
+*/
+int strlen_no_wilds(char *str)
+{
+int len = 0, index = 0;
+}
