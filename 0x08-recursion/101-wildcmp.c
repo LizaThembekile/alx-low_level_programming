e06d83ecf0fa1251534afbcbe8e9fd2e69f58744
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-28 14:38:00 -0700

    fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,11 +8,10 @@ int wildcmp(char *s1, char *s2);
 /**
 * strlen_no_wilds - Returns the length of a string
 *
-*@str: the string to be measured
+* @str: the string to be measured
 *
 * Return: the length.
 */
 int strlen_no_wilds(char *str)
 {
 int len = 0, index = 0;
-}
