0098e63baceb734c91920d5a5bc2b347e09eeb48
Thembekile Liza Phethe <thembekile15@gmail.com>
2022-09-28 14:17:12 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
+	else
+	{
+	_putchar(s[0]);
 	_puts_recursion(s + 1);
+	}
 }
